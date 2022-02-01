0474f009ef0dfc2f776c943b10e40435f15f9094
John Leward Escote <jlescote.halcyondigital@gmail.com>
2022-02-01 14:54:04 +0800

    test
    Signed-off-by: John Leward Escote <jlescote.halcyondigital@gmail.com>

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,6 +3,7 @@
 use Jeyelscott\MyFirstPackage\MyFirstPackageClass;
 
 it('can convert kilometers to miles', function () {
+
     $miles = MyFirstPackageClass::kilometers(100)->toMiles();
 
     expect($miles)->toEqual(62.137);
